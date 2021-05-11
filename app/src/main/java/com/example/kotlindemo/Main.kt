ec49481d08ba752bd0d50941ecd4b1308f17fa8e
Prabhanjan <souderthi24@yahoo.com>
2021-05-11 20:13:55 +0530

    Program1

--- app/src/main/java/com/example/kotlindemo/Main.kt
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -1,5 +1,40 @@
 package com.example.kotlindemo
 
-fun main(){
-    print("Hello world")
+import kotlin.reflect.typeOf
+
+//fun main() {
+// val i:Int = 2
+//// print(i++)
+//// val is a constant variable which cannot be reassigned
+//// var is mutable variable which can be reassigned
+// print("hello added value is ${2+ i}")
+//}
+
+//fun main()
+//{
+//  var x :Int  = 30
+//  when(x){
+//  0-> println("Empty number")
+//  in 1..39 -> println("found")
+//  else ->println("Not found ")
+// }
+//}
+//
+//fun main()
+//{
+// var my_Name:String = "Neil"
+// my_Name = "Hello"
+// print("${my_Name[0]} ${my_Name[my_Name.length -1]}")
+//}
+
+fun main()
+{
+ var x: String = "Andriod Kotlin"
+ var y:Float = 13.37F
+ var z:Double  = 3.14159
+ var w:Int = 25
+ println(x)
+ println(y)
+ println(z)
+ println(w)
 }
